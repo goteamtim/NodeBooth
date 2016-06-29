da1f14440c7479f908bfd45ce1e06a0d3a219dc8
Tim Harshman <goteamtim+git@gmail.com>
2016-06-29 09:24:09 -0700

    use setIntervalID

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function takePictures(){
     let time = new Date().getTime();
+    //setInterval and use the interval id to stop it?
+}
+
+function takePicture(time){
     $.get("localhost:3000/takePictures/"+time, function(data, status){
         
     });
